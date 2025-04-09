56d40644f1610af560cd627a27c5be6cacc3c974
thatsmanmeet <120583077+thatsmanmeet@users.noreply.github.com>
2025-04-09 22:48:30 +0530

    created orders model

---
+++ backend/models/orders.models.js
@@ -0,0 +1,86 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    shippingAddress: {
+      house: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: true },
+      zipcode: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      enum: {
+        values: ['Card', 'COD'],
+      },
+      default: 'COD',
+      required: true,
+    },
+    paymentStatus: {
+      id: String,
+      email: String,
+      status: String,
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+    updates: [
+      {
+        date: { type: Date, default: Date.now },
+        message: { type: String },
+      },
+    ],
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+        },
+        quantity: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
