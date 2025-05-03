c614965217623843a5812726df2d6affeb7c7cbf
thatsmanmeet <120583077+thatsmanmeet@users.noreply.github.com>
2025-05-03 19:31:53 +0530

    disabled console logs

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -16,7 +16,7 @@ let refreshing = null;
 
 async function baseQueryWithAuth(args, api, extra) {
   let result = await baseQuery(args, api, extra);
-  console.log(result);
+  // console.log(result);
   if (result.error?.originalStatus === 429) {
     window.location.href = '/429';
     return result;
@@ -35,7 +35,7 @@ async function baseQueryWithAuth(args, api, extra) {
     typeof result.error.data?.message === 'string' &&
     result.error.data.message.includes('expired')
   ) {
-    console.log('refresh token expired');
+    // console.log('refresh token expired');
     if (!refreshing) {
       refreshing = (async () => {
         const refreshRes = await baseQuery(
@@ -45,7 +45,7 @@ async function baseQueryWithAuth(args, api, extra) {
         );
         if (refreshRes.data?.data) {
           api.dispatch(setCredentialsOnLogin(refreshRes.data.data));
-          console.log('refresh token refreshed', refreshRes.data.data);
+          // console.log('refresh token refreshed', refreshRes.data.data);
         } else {
           api.dispatch(removeCredentialsOnLogout());
         }
